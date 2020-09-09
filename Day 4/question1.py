6a460dc76671da237cbe17fcfaf999fa3d6577e3
Arnav <arnavsinha23456789@gmail.com>
2020-09-09 21:07:41 +0530

    Add files via upload

---
+++ Day 4/question1.py
@@ -0,0 +1,14 @@
+l=1042000
+u=702648265
+while l<=u:
+    order=len(str(l))
+    sum=0
+    a=l
+    while a>0:
+        d=a%10
+        sum+=d**order
+        a//=10
+    if l==sum:
+        print(l)
+        break
+    l+=1
